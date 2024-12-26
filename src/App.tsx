715e8e0ff06a326f09de736a95ef8769deff21b0
Samet Orbuk <orbuk60@gmail.com>
2024-12-26 19:24:33 +0300

    Redux toolkit setted and tried

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
+import { useDispatch } from "react-redux";
 import "./App.css";
+import { useSelector } from "react-redux";
+import { updateUser } from "./slices/userSlice";
+import { RootState } from "./store/store";
 
 function App() {
+  const dispatch = useDispatch();
+
+  const user = useSelector((state: RootState) => state.user);
+
+  dispatch(
+    updateUser({
+      name: "hakan",
+    })
+  );
+
   return (
     <>
-      <h2 className="font-bold text-3xl text-red-500">SAMET</h2>
+      <h2 className="font-bold text-3xl text-red-500">{user.name}</h2>
     </>
   );
 }
---
+++ src/slices/userSlice.ts
@@ -0,0 +1,76 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+interface UserState {
+  id: number | null;
+  name: string | null;
+  surname: string | null;
+  phoneNumber: string | null;
+  email: string | null;
+  token: string | null;
+}
+
+const initialState: UserState = {
+  id: null,
+  name: null,
+  surname: null,
+  phoneNumber: null,
+  email: null,
+  token: null,
+};
+
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    login: (
+      state,
+      action: PayloadAction<{
+        id: number;
+        name: string;
+        email: string;
+        token: string;
+        phoneNumber: string;
+      }>
+    ) => {
+      state.id = action.payload.id;
+      state.name = action.payload.name;
+      state.email = action.payload.email;
+      state.token = action.payload.token;
+      state.phoneNumber = action.payload.phoneNumber;
+    },
+
+    logout: (state) => {
+      state.email = null;
+      state.id = null;
+      state.phoneNumber = null;
+      state.surname = null;
+      state.token = null;
+      state.name = null;
+    },
+
+    updateUser: (
+      state,
+      action: PayloadAction<{
+        id?: number;
+        name?: string;
+        email?: string;
+        surname?: string;
+        token?: string;
+        phoneNumber?: string;
+      }>
+    ) => {
+      const updates = action.payload;
+
+      for (const key in updates) {
+        if (Object.prototype.hasOwnProperty.call(updates, key)) {
+          state[key as keyof typeof updates] =
+            updates[key as keyof typeof updates];
+        }
+      }
+    },
+  },
+});
+
+export const { login, logout, updateUser } = userSlice.actions;
+
+export default userSlice.reducer;
